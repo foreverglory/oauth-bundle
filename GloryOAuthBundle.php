07d87818d549a0da6d653d2189c6c7af7e11b173
ForeverGlory <foreverglory@qq.com>
2016-05-09 23:43:43 +0800

    add OAuthFactory Signed-off-by:ForeverGlory <foreverglory@qq.com>

--- GloryOAuthBundle.php
+++ GloryOAuthBundle.php
@@ -3,7 +3,39 @@
 namespace Glory\Bundle\OAuthBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Glory\Bundle\OAuthBundle\DependencyInjection\GloryOAuthExtension;
+use Glory\Bundle\OAuthBundle\DependencyInjection\Security\Factory\OAuthFactory;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class GloryOAuthBundle extends Bundle
 {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        /** @var $extension SecurityExtension */
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new OAuthFactory());
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContainerExtension()
+    {
+        // return the right extension instead of "auto-registering" it. Now the
+        // alias can be glory_oauth instead of glory_o_auth..
+        if (null === $this->extension) {
+            return new GloryOAuthExtension();
+        }
+
+        return $this->extension;
+    }
+
 }
