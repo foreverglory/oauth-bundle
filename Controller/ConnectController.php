fe89aaf753066fe03cee9976d735a5f715e12473
ForeverGlory <foreverglory@qq.com>
2016-05-13 21:29:49 +0800

    第三方新用户注册,老用户登录 Signed-off-by:ForeverGlory <foreverglory@qq.com>

---
+++ Controller/ConnectController.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ */
+
+namespace Glory\Bundle\OAuthBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of ConnectController
+ *
+ * @author ForeverGlory
+ */
+class ConnectController extends Controller
+{
+
+    public function registerAction(Request $request, $key)
+    {
+        
+    }
+
+    public function loginAction(Request $request, $key)
+    {
+        
+    }
+
+    public function successAction(Request $request)
+    {
+        
+    }
+
+}
