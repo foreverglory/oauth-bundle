3716613c819a399864fc94db8673ae4968be1b0e
ForeverGlory <foreverglory@qq.com>
2016-05-13 20:55:32 +0800

    glory_oauth.util.token2oauth service Signed-off-by:ForeverGlory <foreverglory@qq.com>

---
+++ Util/Token2OAuth.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ */
+
+namespace Glory\Bundle\OAuthBundle\Util;
+
+use Glory\Bundle\OAuthBundle\Model\OAuthInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
+
+/**
+ * Description of Token to OAuth
+ *
+ * @author ForeverGlory
+ */
+class Token2OAuth
+{
+
+    protected $container;
+    protected $hasOauth = false;
+    protected $oauth;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function handle(OAuthToken $token)
+    {
+        $ownerMap = $this->container->get('glory_oauth.ownerMap');
+        $oauthManager = $this->container->get('glory_oauth.oauth_manager');
+        $owner = $ownerMap->getOwner($token->getResourceOwnerName());
+        $userInformation = $owner->getUserInformation($token->getRawToken());
+        $oauth = $oauthManager->findOAuthBy(['' => $userInformation['']]);
+        if (!$this->hasOauth = (boolean) $oauth) {
+            $oauth = $oauthManager->createOAuth();
+        }
+        $this->oauth = $this->bindOAuth($oauth, $userInformation);
+    }
+
+    public function bindOAuth(OAuthInterface $oauth, $userInfomation)
+    {
+        return $oauth;
+    }
+
+    public function hasOAuth()
+    {
+        return $this->hasOauth;
+    }
+
+    /**
+     * @return OAuthInterface
+     */
+    public function getOAuth()
+    {
+        return $this->oauth;
+    }
+
+}
