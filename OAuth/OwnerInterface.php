a330d6bd3daabb903e87e4e0cc589eb2c5b8f98a
ForeverGlory <foreverglory@qq.com>
2016-06-24 17:16:30 +0800

    继承，以便重写

---
+++ OAuth/OwnerInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\OAuthBundle\OAuth;
+
+use HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface;
+
+/**
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+interface OwnerInterface extends ResourceOwnerInterface
+{
+    //put your code here
+}
