2e690323b72e1eef16f9cb21ca968579b7bbca3c
ForeverGlory <foreverglory@qq.com>
2016-06-24 22:58:05 +0800

    修复 wechat owner @see https://github.com/hwi/HWIOAuthBundle/pull/1000

---
+++ OAuth/Owner/WechatOwner.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\OAuthBundle\OAuth\Owner;
+
+use HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\WechatResourceOwner;
+
+/**
+ * Description of WechatOwner
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class WechatOwner extends WechatResourceOwner
+{
+
+    public function getAccessToken(Request $request, $redirectUri, array $extraParameters = array())
+    {
+        $parameters = array_merge(array(
+            'appid' => $this->options['client_id'],
+            'secret' => $this->options['client_secret'],
+            'code' => $request->query->get('code'),
+            'grant_type' => 'authorization_code',
+                ), $extraParameters);
+
+        $response = $this->doGetTokenRequest($this->options['access_token_url'], $parameters);
+        $response = $this->getResponseContent($response);
+
+        $this->validateResponseContent($response);
+
+        return $response;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getUserInformation(array $accessToken = null, array $extraParameters = array())
+    {
+        if ('snsapi_userinfo' === $this->options['scope']) {
+            $openid = $accessToken['openid'];
+
+            $url = $this->normalizeUrl($this->options['infos_url'], array(
+                'access_token' => $accessToken['access_token'],
+                'openid' => $openid,
+            ));
+
+            $response = $this->doGetUserInformationRequest($url);
+            $content = $this->getResponseContent($response);
+        } else {
+            $content = array(
+                'openid' => $accessToken['openid'],
+            );
+        }
+
+        $this->validateResponseContent($content);
+
+        $response = $this->getUserResponse();
+        $response->setResponse($content);
+        $response->setResourceOwner($this);
+        $response->setOAuthToken(new OAuthToken($accessToken));
+
+        return $response;
+    }
+
+}
